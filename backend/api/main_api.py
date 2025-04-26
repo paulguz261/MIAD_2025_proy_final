99a95d778805afd846c79609b648b82a7288e608
jhoceldsuescuntorres <148110019+jhoceldsuescuntorres@users.noreply.github.com>
2025-04-26 11:47:35 -0500

    Feature/enable api rest (#1)
    * Se agrega file .gitignore para excluir archivos de python de cache
    
    * Se agrega el model con los clientes a utilizar
    
    * Se define el folder para manejo del template
    
    * Se habilita la api a utilizar
    
    * Se actualizan los modelos de prediccion
    
    * Se actualiza el gitignore para excluir los archivos de cache de python
    
    * Se reubican de directorio los archivos .html

--- backend/api/main_api.py
+++ backend/api/main_api.py
@@ -1,7 +1,7 @@
 # main_api.py
 
-from fastapi import FastAPI, Query
-from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
 import os
 import sys
 import pandas as pd
@@ -10,20 +10,22 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import contugas_anomaly_project.preprocess as pre
 import contugas_anomaly_project.config as cnf
 import contugas_anomaly_project.model as mdl
+from data.clientes import get_all_clientes
+from datetime import datetime
+import plotly.graph_objs as go
+import plotly.io as pio
+ 
+# Obtener la ruta absoluta a la carpeta 'templates' fuera de 'backend/api'
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../templates'))
 
-app = FastAPI()
-
+# Crear la instancia de Flask y especificar la carpeta de plantillas
+app = Flask(__name__, template_folder=template_dir)
 # Allow frontend (JavaScript) to connect
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # you can restrict to your frontend later
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+CORS(app) 
 
-@app.get("/api/client-data")
-def get_client_data(cliente: str):
+@app.route("/api/client-data", methods=['GET'])
+def get_client_data():
+    cliente = request.args.get('cliente')
     # Load the full dataset
     df = pre.cargar_datos(cnf.DIR_PROCESSED_FILE)
     
@@ -34,12 +36,79 @@ def get_client_data(cliente: str):
     df_cliente = pre.preprocesa_datos(df_cliente)
 
     # Load the model
-    model_path = os.path.join(cnf.MODEL_DIR,)
-    model_path = f"{cliente}_pipeline.pkl"
+    model_path = os.path.join(cnf.MODEL_DIR, f"{cliente}_pipeline.pkl")
     pipeline = joblib.load(model_path)
 
     # Predict anomalies
     df_result = mdl.predict_with_pipeline(pipeline, df_cliente)
+    
+    return jsonify({"resultados":df_result[["Fecha", "Presion", "Temperatura", "Volumen", "anomaly_label"]].to_dict(orient="records")})
 
-    # Return only needed columns as JSON
-    return df_result[["Fecha", "Presion", "Temperatura", "Volumen", "anomaly_label"]].to_dict(orient="records")
+
+# Función para cargar el modelo
+def load_model(cliente):
+    model_path = os.path.join(cnf.MODEL_DIR, f"{cliente}_pipeline.pkl")
+    model = joblib.load(model_path)
+    return model
+
+@app.route('/api/predict', methods=['GET'])
+def predict():
+    # Recibimos los parámetros de la URL
+    fecha = request.args.get('fecha')  # "2023-12-31 19:00:00"
+    presion = request.args.get('presion')  # Ejemplo: 1015
+    temperatura = request.args.get('temperatura')  # Ejemplo: 25
+    volumen = request.args.get('volumen')  # Ejemplo: 500
+    cliente = request.args.get('cliente', 'CLIENTE1')  # Si no se pasa, tomamos 'CLIENTE1' por defecto
+
+    # Load the full dataset
+    df = pre.cargar_datos(cnf.DIR_PROCESSED_FILE)
+    
+    # Filter by client
+    df_cliente = df[df["cliente"] == cliente]
+    nuevo = pd.DataFrame([{'Fecha': pd.to_datetime(fecha), 'Presion': float(presion), 'Temperatura': float(temperatura), 'Volumen': float(volumen), 'cliente': cliente}])
+    df_cliente = pd.concat([df_cliente, nuevo], ignore_index=True)
+    # Preprocess it
+    df_cliente = pre.preprocesa_datos(df_cliente)
+
+    # Load the model
+    model_path = os.path.join(cnf.MODEL_DIR, f"{cliente}_pipeline.pkl")
+    pipeline = joblib.load(model_path)
+
+    # Predict anomalies
+    df_result = mdl.predict_with_pipeline(pipeline, df_cliente)
+    
+    #return jsonify({"resultados":df_result[["Fecha", "Presion", "Temperatura", "Volumen", "anomaly_label"]].to_dict(orient="records")})
+    clientes = get_all_clientes()
+    return render_template("dashboard_v1.html", clientes=clientes, grafico_html=grafico(df_result))
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("dashboard.html")
+
+@app.route("/v1", methods=["GET"])
+def indexV1():
+    clientes = get_all_clientes()
+    return render_template("dashboard_v1.html", clientes=clientes, grafico_html=grafico())
+
+@app.route("/v2", methods=["GET"])
+def indexV2():
+    return render_template("dashboard_v2.html")
+
+def grafico(data):
+  
+    # Crear la figura con varias líneas
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Fecha'], y=data['Presion'], mode='lines+markers', name='Presión'))
+    fig.add_trace(go.Scatter(x=data['Fecha'], y=data['Temperatura'], mode='lines+markers', name='Temperatura'))
+    fig.add_trace(go.Scatter(x=data['Fecha'], y=data['Volumen'], mode='lines+markers', name='Volumen'))
+
+    fig.update_layout(title="Presión, Temperatura y Volumen a lo largo del tiempo",
+                      xaxis_title="Fecha",
+                      yaxis_title="Valor",
+                      legend_title="Variables")
+
+    # Convertir el gráfico a HTML
+    return pio.to_html(fig, full_html=False)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/data/clientes.py
@@ -0,0 +1,27 @@
+# Diccionario de clientes
+clientes = {
+    'CLIENTE1': {'Nombre': 'CLIENTE1'},
+    'CLIENTE2': {'Nombre': 'CLIENTE2'},
+    'CLIENTE3': {'Nombre': 'CLIENTE3'},
+    'CLIENTE4': {'Nombre': 'CLIENTE4'},
+    'CLIENTE5': {'Nombre': 'CLIENTE5'},
+    'CLIENTE6': {'Nombre': 'CLIENTE6'},
+    'CLIENTE7': {'Nombre': 'CLIENTE7'},
+    'CLIENTE8': {'Nombre': 'CLIENTE8'},
+    'CLIENTE9': {'Nombre': 'CLIENTE9'},
+    'CLIENTE10': {'Nombre': 'CLIENTE10'},
+    'CLIENTE11': {'Nombre': 'CLIENTE11'},
+    'CLIENTE12': {'Nombre': 'CLIENTE12'},
+    'CLIENTE13': {'Nombre': 'CLIENTE13'},
+    'CLIENTE14': {'Nombre': 'CLIENTE14'},
+    'CLIENTE15': {'Nombre': 'CLIENTE15'},
+    'CLIENTE16': {'Nombre': 'CLIENTE16'},
+    'CLIENTE17': {'Nombre': 'CLIENTE17'},
+    'CLIENTE18': {'Nombre': 'CLIENTE18'},
+    'CLIENTE19': {'Nombre': 'CLIENTE19'},
+    'CLIENTE20': {'Nombre': 'CLIENTE20'}
+}
+
+# También puedes agregar funciones relacionadas si quieres
+def get_all_clientes():
+    return clientes
